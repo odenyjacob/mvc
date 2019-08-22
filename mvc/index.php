6eaf6350cd5082bb06592d70d98d6dfc2a1a0129
Odeny Oyoko <50321211+odenyjacob@users.noreply.github.com>
2019-08-22 23:24:48 +0300

    Delete index.php

--- mvc/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//$url = $_GET['url'];
-$url = filter_input(INPUT_GET,'url');
-
-echo $url;
-
-//require 'controllers/'.'url'.'.php';
-require 'controllers/'.$url.'.php';
-//require 'controllers/'.'index.php';
